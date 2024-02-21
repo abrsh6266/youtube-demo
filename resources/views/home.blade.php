7a6d7d0af8e1cce0b2f00788ab55046c07eed08e
Abrham <abrsh6266@gmail.com>
2024-02-20 21:10:35 -0900

    creating home view and user controller files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div style="border: 3px solid black;">
+        <h2>Register</h2>
+        <form action="/register" method="POST">
+            @csrf
+            <input type="text" placeholder="name" name="name">
+            <input type="text" placeholder="email" name="email">
+            <input type="password" placeholder="password">
+            <button>Register</button>
+        </form>
+    </div>
+</body>
+
+</html>
