10c51156e90de7e1528ef3d5761fe36aa91dda38
Abrham <abrsh6266@gmail.com>
2024-02-21 01:04:35 -0900

    creating post model and configuring it

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class postController extends Controller
 {
-    //
+    public function createPost(Request $request){
+        $incomingRequest = $request->validate([
+            "title"=> "required",
+            "body" => "required",
+        ]);
+        $incomingRequest['title'] = strip_tags($incomingRequest['title']);
+        $incomingRequest['body'] = strip_tags($incomingRequest['body']);
+        $incomingRequest['user_id'] = auth()->id();
+        Post::create($incomingRequest);
+        return redirect('/');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\postController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +21,4 @@ Route::get('/', function () {
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/logout', [UserController::class, 'logout']);
 Route::post('/login', [UserController::class, 'login']);
-Route::post('/create-post', [UserController::class, 'login']);
+Route::post('/create-post', [postController::class, 'createPost']);
